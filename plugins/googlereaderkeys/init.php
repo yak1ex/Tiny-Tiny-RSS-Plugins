bc3c47e55934d422e805ca8ce543f97b3ff4c473
Yak! <yak_ex@mx.scn.tv>
2013-06-27 09:38:27 +0900

    Add more google reader keys.

--- plugins/googlereaderkeys/init.php
+++ plugins/googlereaderkeys/init.php
@@ -3,7 +3,7 @@ class GoogleReaderKeys extends Plugin {
 	private $host;
 
 	function about() {
-		return array(1.0,
+		return array(1.1,
 			"Keyboard hotkeys emulate Google Reader",
 			"markwaters");
 	}
@@ -16,19 +16,29 @@ class GoogleReaderKeys extends Plugin {
 
 	function hook_hotkey_map($hotkeys) {
 
-		$hotkeys["j"]		= "next_article_noscroll";
-		$hotkeys["k"]		= "prev_article_noscroll";
-		$hotkeys["*n"]		= "next_feed";
-		$hotkeys["*p"]		= "prev_feed";
+//		             		                           // original key binding
+		$hotkeys["j"]		= "next_article_noscroll"; // prev_feed
+		$hotkeys["k"]		= "prev_article_noscroll"; // next_feed
+		$hotkeys["n"]		= "next_article_noexpand"; // next_article
+		$hotkeys["p"]		= "prev_article_noexpand"; // prev_article
+		$hotkeys["*n"]		= "next_feed";             // article_scroll_down
+		$hotkeys["*p"]		= "prev_feed";             // article_scroll_up
 		$hotkeys["v"]		= "open_in_new_window";
 		$hotkeys["r"]		= "feed_refresh";
+//		$hotkeys["s"]		= "toggle_mark";           // no change
+//		$hotkeys["*s"]		= "toggle_publ";           // no change
 		$hotkeys["m"]		= "toggle_unread";
-		$hotkeys["o"]		= "toggle_expand";
+		$hotkeys["t"]		= "edit_tags";
+		$hotkeys["o"]		= "toggle_expand";         // open_in_new_window
 		$hotkeys["(13)|enter"]	= "toggle_expand";
+		$hotkeys["*a"]		= "feed_catchup";
+		$hotkeys["1"]		= "toggle_cdm_expanded";
+		$hotkeys["2"]		= "toggle_cdm_expanded";
+		$hotkeys["r"]		= "feed_refresh";
 		$hotkeys["*(191)|?"]    = "help_dialog";
 		$hotkeys["(32)|space"]	= "next_article";
-		$hotkeys["(38)|up"]	= "article_scroll_up";
-		$hotkeys["(40)|down"]	= "article_scroll_down";
+		$hotkeys["(38)|up"]	= "article_scroll_up";     // prev_article
+		$hotkeys["(40)|down"]	= "article_scroll_down";   // next_article
 
 		return $hotkeys;
 	}
