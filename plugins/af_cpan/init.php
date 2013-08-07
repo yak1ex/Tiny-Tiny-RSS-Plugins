c056981c0c9b505c4dd6eb294fabc4b36aaba1df
Yak! <yak_ex@mx.scn.tv>
2013-08-07 09:09:48 +0900

    Add a plugin to embed Changes for CPAN modules (close gh-1).

---
+++ plugins/af_cpan/init.php
@@ -0,0 +1,51 @@
+<?php
+class Af_Cpan extends Plugin {
+
+	private $host;
+
+	function about() {
+		return array(1.0,
+			"Include CPAN Changes",
+			"yak_ex");
+	}
+
+	function init($host) {
+		$this->host = $host;
+
+		$host->add_hook($host::HOOK_ARTICLE_FILTER, $this);
+	}
+
+	function hook_article_filter($article) {
+		$owner_uid = $article["owner_uid"];
+
+		if (strpos($article["guid"], "search.cpan.org") !== FALSE) {
+			if (strpos($article["plugin_data"], "cpan,$owner_uid:") === FALSE) {
+
+				$changes = fetch_file_contents($article["link"] . 'Changes');
+				if (! $changes) {
+					$changes = fetch_file_contents($article["link"] . 'ChangeLog');
+				}
+				if ($changes) {
+					$lines = explode("\n", $changes);
+					if (count($lines) > 20) {
+						$article["content"] = "<pre>" . implode("\n", array_slice($lines, 0, 20)) . "\n...</pre>";
+					} else {
+						$article["content"] = "<pre>" . $changes . "</pre>";
+					}
+					$article["plugin_data"] = "cpan,$owner_uid:" . $article["plugin_data"];
+				}
+
+			} else if (isset($article["stored"]["content"])) {
+				$article["content"] = $article["stored"]["content"];
+			}
+		}
+
+		return $article;
+	}
+
+	function api_version() {
+		return 2;
+	}
+
+}
+?>
